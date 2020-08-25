03f3c50a04527a7e64f5210ce5d9c8db87762c65
Nicolas Ferey <nicolasferey@limsi.fr>
2020-08-25 16:33:45 +0200

    Cleaning MDDriver NF

--- protocol/src/python/mddriver1.0/mddriver/mddriver.c
+++ protocol/src/python/mddriver1.0/mddriver/mddriver.c
@@ -2,15 +2,11 @@
 
 #include "imd_interface.h"
 
-PyDoc_STRVAR(mddriver__doc__,
-			 "Python interface to MDDriver");
+PyDoc_STRVAR(mddriver__doc__,"Python interface to MDDriver");
 
+PyDoc_STRVAR(mddriver_client__doc__,"Must be call before by the client before starting a interactive molecular dynamics. ");
 
-PyDoc_STRVAR(mddriver_client__doc__,
-			 "Must be call before by the client before starting a interactive molecular dynamics. ");
-
-PyDoc_STRVAR(mddriver_server__doc__,
-			 "Must be call before by the server (simulation code) and by the client before starting a interactive molecular dynamics. ");
+PyDoc_STRVAR(mddriver_server__doc__,"Must be call before by the server (simulation code) and by the client before starting a interactive molecular dynamics. ");
 
 PyDoc_STRVAR(mddriver_probeconnection__doc__, "mddriver_probeconnection__doc__");
 PyDoc_STRVAR(mddriver_treatprotocol__doc__, "mddriver_treatprotocol__doc__");
@@ -41,14 +37,13 @@ static PyObject * mddriver_client(PyObject *self, PyObject *args)
 		{
 		return NULL;
 		}
-	IIMD_init(hostname, &mode, &wait, &port,  &message, filename );	
+	IIMD_init(hostname, &mode, &wait, &port,  &message, filename );
 	Py_INCREF(Py_None);
 	return Py_None;
 	}
 
 static PyObject * mddriver_server(PyObject *self, PyObject *args)
 	{
-	
 	char * filename;
 	int mode=1;
 	int wait=0;
@@ -58,7 +53,7 @@ static PyObject * mddriver_server(PyObject *self, PyObject *args)
 		{
 		return NULL;
 		}
-	IIMD_init("", &mode, &wait, &port,  &message, filename );	
+	IIMD_init("", &mode, &wait, &port,  &message, filename );
 	Py_INCREF(Py_None);
 	return Py_None;
 	}
@@ -72,7 +67,7 @@ static PyObject * mddriver_probeconnection(PyObject *self, PyObject *args)
 
 static PyObject * mddriver_treatprotocol(PyObject *self, PyObject *args)
 	{
-	IIMD_treatprotocol     ( );	
+	IIMD_treatprotocol     ( );
 	Py_INCREF(Py_None);
 	return Py_None;
 	}
@@ -93,7 +88,7 @@ static PyObject * mddriver_sendpause(PyObject *self, PyObject *args)
 	return Py_None;
 	}
 
-static PyObject * mddriver_sendkill(PyObject *self, PyObject *args)	
+static PyObject * mddriver_sendkill(PyObject *self, PyObject *args)
 	{
 	IIMD_send_kill        ( );
 	Py_INCREF(Py_None);
@@ -110,13 +105,13 @@ static PyObject * mddriver_terminate(PyObject *self, PyObject *args)
 static PyObject * mddriver_getforces(PyObject *self, PyObject *args)
 	{
 	int nbatoms;
-	imd_int32 * atomids; 
-	float * atomforces; 		
-	IIMD_get_forces        ( &nbatoms, &atomids, &atomforces);	
+	imd_int32 * atomids;
+	float * atomforces;
+	IIMD_get_forces        ( &nbatoms, &atomids, &atomforces);
 	PyObject* dict=PyDict_New();
 	if (!dict)
 		return NULL;
-	
+
 	unsigned i=0;
 	for(i=0;i<nbatoms;i++)
 		{
@@ -134,20 +129,20 @@ static PyObject * mddriver_sendcoords(PyObject *self, PyObject *args)
 		{
 		return NULL;
 		}
-	else 
+	else
 		{
-		int i, n; 
-		PyObject *item; 
-		n = PyList_Size(poslist); 
-				
+		int i, n;
+		PyObject *item;
+		n = PyList_Size(poslist);
+
 		if (n < 0)
-			return NULL;; /* Not a list */ 
+			return NULL;; /* Not a list */
 		float * posatoms=(float *) malloc (n*sizeof(float)*3);
 		memset(posatoms, 0, n*sizeof(float)*3);
-		for (i = 0; i < n; i++) 
+		for (i = 0; i < n; i++)
 			{
-			item = PyList_GetItem(poslist, i); /* Can’t fail */ 
-			if (!PyTuple_Check(item)) 
+			item = PyList_GetItem(poslist, i); /* Can’t fail */
+			if (!PyTuple_Check(item))
 				{
 				printf("Erreur");
 				continue;
@@ -162,7 +157,7 @@ static PyObject * mddriver_sendcoords(PyObject *self, PyObject *args)
 			posatoms[i*3+2]=  PyFloat_AsDouble(PyTuple_GetItem(item, 2));
 			}
 		IIMD_send_coords( &n , posatoms );
-		
+
 		free(posatoms);
 		Py_INCREF(Py_None);
 		return Py_None;
@@ -176,23 +171,23 @@ static PyObject * mddriver_sendcurrentenergies(PyObject *self, PyObject *args)
 		{
 		return NULL;
 		}
-	
+
 	if (!PyDict_Check(energiesdict))
 		{
 		printf("Erreur");
 		return NULL;
-		}	
-	
+		}
+
 	IMDEnergies energies;
 
 	memset(&energies,0, sizeof(energies) );
-	
+
 	PyObject * energy=NULL;
 	PyObject * enertype=NULL;
-	
+
 	Py_ssize_t it=0;
 	unsigned i=0;
-	
+
 	while(PyDict_Next(energiesdict, &it, &enertype, &energy))
 		{
 		if (!PyString_Check(enertype))
@@ -206,78 +201,78 @@ static PyObject * mddriver_sendcurrentenergies(PyObject *self, PyObject *args)
 			if (!PyInt_Check(energy))
 				{
 				return NULL;
-				}	
+				}
 			energies.tstep  = PyInt_AsLong(energy);
-			}	
+			}
 
 		if (strcmp(typestr, "T")==0)
 			{
 			if (!PyFloat_Check(energy))
 				{
 				return NULL;
-				}	
+				}
 			energies.T      = PyFloat_AsDouble(energy);
-			}	
+			}
 
 		if (strcmp(typestr, "Etot")==0)
 			{
 			if (!PyFloat_Check(energy))
 				{
 				return NULL;
-				}	
+				}
 			energies.Etot      = PyFloat_AsDouble(energy);
-			}	
+			}
 
 		if (strcmp(typestr, "Epot")==0)
 			{
 			if (!PyFloat_Check(energy))
 				{
 				return NULL;
-				}	
+				}
 			energies.Epot      = PyFloat_AsDouble(energy);
-			}	
+			}
 
 		if (strcmp(typestr, "Evdw")==0)
 			{
 			if (!PyFloat_Check(energy))
 				{
 				return NULL;
-				}	
+				}
 			energies.Evdw      = PyFloat_AsDouble(energy);
-			}	
+			}
 
 		if (strcmp(typestr, "Eelec")==0)
 			{
 			if (!PyFloat_Check(energy))
 				{
 				return NULL;
-				}	
+				}
 			energies.Eelec      = PyFloat_AsDouble(energy);
-			}	
+			}
 
 		if (strcmp(typestr, "Eangle")==0)
 			{
 			if (!PyFloat_Check(energy))
 				{
 				return NULL;
-				}	
+				}
 			energies.Eangle      = PyFloat_AsDouble(energy);
-			}	
+			}
 
 		if (strcmp(typestr, "Edihe")==0)
 			{
 			if (!PyFloat_Check(energy))
 				{
 				return NULL;
-				}	
+				}
 			energies.Edihe      = PyFloat_AsDouble(energy);
-			}	
+			}
 		if (strcmp(typestr, "Eimpr")==0)
 			{
 			if (!PyFloat_Check(energy))
 				{
 				return NULL;
-				}	
+				}
 			energies.Eimpr      = PyFloat_AsDouble(energy);
 			}
 		i++;
@@ -306,13 +301,13 @@ static PyObject * mddriver_sendtrate(PyObject *self, PyObject *args)
 	IIMD_send_trate( &nbstep);
 	Py_INCREF(Py_None);
 	return Py_None;
-	}	
+	}
 
 static PyObject * mddriver_getcoords(PyObject *self, PyObject *args)
 	{
 	imd_int32 nbatoms=0;
-	float * posatoms=NULL;	
-	
+	float * posatoms=NULL;
+
 	IIMD_get_coords( &nbatoms, &posatoms);
 	printf("nb atoms %d\n", nbatoms);
 	PyObject *lst = PyList_New(nbatoms);
@@ -321,14 +316,14 @@ static PyObject * mddriver_getcoords(PyObject *self, PyObject *args)
 		return NULL;
 		}
 	unsigned i=0;
-	for (i = 0; i < nbatoms; i++) 
+	for (i = 0; i < nbatoms; i++)
 		{
 		PyObject * pos=Py_BuildValue("(fff)", posatoms[i*3],posatoms[i*3+1],posatoms[i*3+2] );
 
 		PyList_SetItem(lst, i, pos);
 		}
 	return lst;
-		}	
+		}
 
 
 static PyObject * mddriver_sendforces(PyObject *self, PyObject *args)
@@ -338,38 +333,38 @@ static PyObject * mddriver_sendforces(PyObject *self, PyObject *args)
 		{
 		return NULL;
 		}
-	
+
 	if (!PyDict_Check(forcesdict))
 		{
 		printf("Erreur");
 		return NULL;
-		}	
-	
-	int nbforces = PyDict_Size(forcesdict); 
+		}
+
+	int nbforces = PyDict_Size(forcesdict);
 
 	if (nbforces < 0)
 		{
-		printf("Erreur");			
-		return NULL;; /* Not a Dict */ 
+		printf("Erreur");
+		return NULL;; /* Not a Dict */
 		}
 	float * forceatoms=(float *) malloc (nbforces*sizeof(float)*3);
-	int * forceatomsids=(int *) malloc (nbforces*sizeof(int));		
+	int * forceatomsids=(int *) malloc (nbforces*sizeof(int));
 	memset(forceatoms, 0, nbforces*sizeof(float)*3);
-	memset(forceatomsids, 0, nbforces*sizeof(int));		
-	
+	memset(forceatomsids, 0, nbforces*sizeof(int));
+
 	PyObject * force=NULL;
 	PyObject * id=NULL;
-	
+
 	Py_ssize_t it=0;
 	unsigned i=0;
 	while(PyDict_Next(forcesdict, &it, &id, &force))
 		{
-		if (!PyTuple_Check(force)) 
+		if (!PyTuple_Check(force))
 			{
 			printf("Erreur");
 			return NULL;
 			}
-		if (!PyInt_Check(id)) 
+		if (!PyInt_Check(id))
 			{
 			printf("Erreur");
 			return NULL;
@@ -381,10 +376,10 @@ static PyObject * mddriver_sendforces(PyObject *self, PyObject *args)
 		i++;
 		}
 	IIMD_send_forces(&nbforces , forceatomsids,  forceatoms);
-	
+
 	free(forceatoms);
 	free(forceatomsids);
-	
+
 	Py_INCREF(Py_None);
 	return Py_None;
 	}
@@ -400,7 +395,7 @@ static PyObject * mddriver_getenergies(PyObject *self, PyObject *args)
 		if (!dict)
 			return NULL;
 
-	
+
 		PyDict_SetItem(dict, PyString_FromString("tstep"),PyInt_FromLong(energies->tstep));
 		PyDict_SetItem(dict, PyString_FromString("T"),PyFloat_FromDouble(energies->T));
 		PyDict_SetItem(dict, PyString_FromString("Etot"),PyFloat_FromDouble(energies->Etot));
@@ -417,7 +412,7 @@ static PyObject * mddriver_getenergies(PyObject *self, PyObject *args)
 		Py_INCREF(Py_None);
 		return Py_None;
 		}
-	
+
 	}
 
 
@@ -460,15 +455,11 @@ static PyMethodDef mddriver_methods[] = {
 	{"mddriver_sendcoords",  mddriver_sendcoords, METH_VARARGS, mddriver_sendcoords__doc__},
 
 	{"mddriver_sendforces",  mddriver_sendforces, METH_VARARGS, mddriver_sendforces__doc__},
-	{"mddriver_getcoords",  mddriver_getcoords, METH_VARARGS, mddriver_getcoords__doc__},	
+	{"mddriver_getcoords",  mddriver_getcoords, METH_VARARGS, mddriver_getcoords__doc__},
 	{"mddriver_sendtrate",  mddriver_sendtrate, METH_VARARGS, mddriver_sendtrate__doc__},
-	{"mddriver_getenergies",  mddriver_getenergies, METH_VARARGS, mddriver_getenergies__doc__},	
-	
-	
+	{"mddriver_getenergies",  mddriver_getenergies, METH_VARARGS, mddriver_getenergies__doc__},
 	{NULL, NULL}      /* sentinel */
 };
-
-
 
 
 /* When Python imports a C module named 'X' it loads the module */
@@ -482,6 +473,5 @@ initmddriver(void)
 {
 	printf("Importing MDDriver\n");
 	/* There have been several InitModule functions over time */
-	Py_InitModule3("mddriver", mddriver_methods,
-                   mddriver__doc__);
+	Py_InitModule3("mddriver", mddriver_methods,mddriver__doc__);
 }
