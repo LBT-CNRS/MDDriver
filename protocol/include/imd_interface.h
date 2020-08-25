81d7a4ef7008be951e84f17bd5759e50ebbcb94d
Nicolas Ferey <nicolasferey@limsi.fr>
2020-08-25 16:34:20 +0200

    Cleaning MDDriver NF

--- protocol/include/imd_interface.h
+++ protocol/include/imd_interface.h
@@ -4,31 +4,31 @@
  * interactive simulations. This software is governed by the CeCILL-C license
  * under French law and abiding by the rules of distribution of free software.
  * You can use, modify and/or redistribute the software under the terms of the
- * CeCILL-C license as circulated by CEA, CNRS and INRIA at the following URL 
+ * CeCILL-C license as circulated by CEA, CNRS and INRIA at the following URL
  * "http://www.cecill.info".
- * 
- * As a counterpart to the access to the source code and rights to copy, 
- * modify and redistribute granted by the license, users are provided only 
- * with a limited warranty and the software's author, the holder of the 
- * economic rights, and the successive licensors have only limited 
+ *
+ * As a counterpart to the access to the source code and rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty and the software's author, the holder of the
+ * economic rights, and the successive licensors have only limited
  * liability.
  *
- * In this respect, the user attention is drawn to the risks associated 
- * with loading, using, modifying and/or developing or reproducing the 
- * software by the user in light of its specific status of free software, 
- * that may mean that it is complicated to manipulate, and that also 
- * therefore means that it is reserved for developers and experienced 
- * professionals having in-depth computer knowledge. Users are therefore 
- * encouraged to load and test the software's suitability as regards their 
- * requirements in conditions enabling the security of their systems and/or 
- * data to be ensured and, more generally, to use and operate it in the 
+ * In this respect, the user attention is drawn to the risks associated
+ * with loading, using, modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean that it is complicated to manipulate, and that also
+ * therefore means that it is reserved for developers and experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and, more generally, to use and operate it in the
  * same conditions as regards security.
  *
- * The fact that you are presently reading this means that you have had 
+ * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-C license and that you accept its terms.
- 
- * References : 
- * If you use this code, could you please cite one of these references : 	
+
+ * References :
+ * If you use this code, could you please cite one of these references :
  * O. Delalande, N. Ferey, G. Grasseau and M. Baaden : "Complex Molecular Assemblies at hand via Interactive Simulations", 2009, Journal of Computational Chemistry 2009.
  * N. Ferey, O. Delalande, G. Grasseau and M. Baaden : "A VR framework for interacting with molecular simulations", 2008, in proceedings of ACM symposium on Virtual reality software and technology (ACM - VRST'08).
  * N. Ferey, O. Delalande, G. Grasseau and M. Baaden : "From Interactive to Immersive Molecular Dynamics", in Proceedings of the international Workshop on Virtual Reality and Physical Simulation (Eurographics - VRIPHYS'08).
@@ -40,7 +40,7 @@
 
 /** @file imd_interface.h
  * @brief Header file for MDDriver interface
- * @author Marc Baaden <baaden@smplinux.de> 
+ * @author Marc Baaden <baaden@smplinux.de>
  * @author Gilles Grasseau <gilles.grasseau@idris.fr>
  * @author Olivier Delalande <olivier.delalande@ibpc.fr>
  * @author Nicolas Ferey <nicolas.ferey@ibpc.fr>
@@ -57,7 +57,7 @@
 extern imd_int32 imd_event;
 extern imd_int64 imd_value;
 
-#ifdef UNDERSCORE 
+#ifdef UNDERSCORE
 	#define  IIMD_init                iimd_init_
 	#define  IIMD_probeconnection     iimd_probeconnection_
 	#define  IIMD_treatprotocol       iimd_treatprotocol_
@@ -86,14 +86,14 @@ extern imd_int64 imd_value;
 #endif
 
 #ifdef __cplusplus
-	extern "C" 
+	extern "C"
 		{
 #endif
 
 		/**
 		Must be call before by the server (simulation code) and by the client
 		before starting a interactive molecular dynamics.
-		@param hostname hostname providing coordinates or forces 
+		@param hostname hostname providing coordinates or forces
 		@param mode set to 0 if this is the client, 1 if server
 		@param IMDwait_ set to 1 to wait for the client connexion before starting molecular simulation, 0 else
 		@param IMDport_ port to listen or send
@@ -103,7 +103,7 @@ extern imd_int64 imd_value;
 		extern FILE *IIMD_init(const char*  hostname, imd_int32   *mode, imd_int32   *IMDwait_,  imd_int32   *IMDport_,  imd_int32   *IMDmsg_, const char*  IMDfilename_ );
 		/**
 		Check for incoming connection.
-		@return if connection failad return 1 else return 0
+		@return if connection failed return 1 else return 0
 		*/
 		extern int   IIMD_probeconnection   ( );
 
@@ -117,7 +117,7 @@ extern imd_int64 imd_value;
 		/**
 		Send all atoms coordinates on network
 		@param N number of atoms of the molecular system
-		@param coords x y z coordinates arrays of all the atoms (the array has n_atoms*3 float) 
+		@param coords x y z coordinates arrays of all the atoms (the array has n_atoms*3 float)
 		*/
 		extern void  IIMD_send_coords       ( const int *N, const float* coords );
 
@@ -133,9 +133,7 @@ extern imd_int64 imd_value;
 		@param tstep energies of the molecular system
 		@param energies array of energies of the molecular system at each timestep
 		*/
-		extern void  IIMD_send_energy_array ( const imd_int32   *tstep, 
-						      const IMDEnergies *energies 
-						    );
+		extern void  IIMD_send_energy_array ( const imd_int32   *tstep, const IMDEnergies *energies );
 
 		/**
 		Send forces apply on the atoms of the molecular system
@@ -188,14 +186,12 @@ extern imd_int64 imd_value;
 		@param force_list x y z force components applying on each atom in the atom_list id array (this array must have n_atoms*3 floats)
 		@return if energy is avalaible return 1 else return 0
 		*/
-		extern int   IIMD_get_forces        ( imd_int32 *n_atoms, imd_int32 **atom_list, float     **force_list);	
+		extern int   IIMD_get_forces        ( imd_int32 *n_atoms, imd_int32 **atom_list, float     **force_list);
 
 		/**
 		Terminate properly a MDDriver connection
 		*/
 		extern void  IIMD_terminate         ( );
-
-
 
 #ifdef __cplusplus
 		}
@@ -206,6 +202,3 @@ extern imd_int64 imd_value;
 /** @mainpage MDDriver Documentation
 *@htmlinclude mddriver.html
 */
-
-
-
